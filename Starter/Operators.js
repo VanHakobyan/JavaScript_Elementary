48cda386ecfbf753756f073812a384a5fdb4ac58
VanHakobyan <van19962013@mail.ru>
2017-03-27 21:46:59 +0400

    add operators

---
+++ Starter/Operators.js
@@ -0,0 +1,13 @@
+// JavaScript source code
+var x = 5;
+var y = "-4";
+resulit = x + +y;
+document.write("x=" + x + "<br>");
+document.write("y=" + y + "<br>");
+document.write("x+y=" + resulit + "<br>");
+var n = 1;
+var m = true;
+document.write("n=" + n + "<br>");
+document.write("m=" + m + "<br>");
+document.write("n==m? -->");
+document.write(n == m);
