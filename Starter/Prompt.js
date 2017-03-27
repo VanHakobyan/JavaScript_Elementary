c0397ad6201d8672d369dbf56ba6ce3753857587
VanHakobyan <van19962013@mail.ru>
2017-03-27 21:02:58 +0400

    add prompt.js

---
+++ Starter/Prompt.js
@@ -0,0 +1,16 @@
+// JavaScript source code
+alert("First Web");
+document.writeln("this first site html and java script");
+prompt("yes or no", "this");
+
+//calculator
+
+var x = prompt("input x", "input");
+var y = prompt("input y", "input");
+var res = x + y;
+document.writeln(res + "  string");
+"</br>"
+var intX = parseInt(x);
+var intY = parseInt(y);
+res = intX + intY;
+document.writeln(res + "  integer");
