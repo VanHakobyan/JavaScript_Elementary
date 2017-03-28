028d49ad9641a33cdb9719f974b4da52931bcee6
VanHakobyan <van19962013@mail.ru>
2017-03-28 20:57:23 +0400

    add day of the week.

--- Logical structures and Loop/SwitchCase.js
+++ Logical structures and Loop/SwitchCase.js
@@ -1,1 +1,24 @@
-// JavaScript source code
+var day = prompt("input day: 1-2-3-4-5-6-7: ", "");
+
+switch (day) {
+    case "1":
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+        document.write("working day");
+        break;
+    case "6":
+    case "7":
+        {
+            document.write("weekend");
+        }
+        break;
+    default:
+        {
+            document.write("You entered a non-existent day of the week.");
+        }
+        break;
+}
+
+    
