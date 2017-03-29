4b90f8f504e5457668a4055ad4b9f4f92afe912a
VanHakobyan <van19962013@mail.ru>
2017-03-29 20:57:24 +0400

    add Array initialization

---
+++ Array/Array initialization.js
@@ -0,0 +1,9 @@
+﻿var array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+document.write(array1);
+document.write("<hr/>");
+var array2 = ["one", "two", "three", "four", "five"];
+document.write(array2);
+document.write("<hr/>");
+var array3 = [1, "two", true];
+document.write(array3);
+document.write("<hr/>");
