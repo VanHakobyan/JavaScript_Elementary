942b1cc2e22981b64691aaa9a2396669aad51e8c
VanHakobyan <van19962013@mail.ru>
2017-03-30 20:54:36 +0400

    add starter

--- Function/starter.js
+++ Function/starter.js
@@ -1,1 +1,9 @@
-// JavaScript source code
+function before(a) {
+    var sum = 0;
+    for (var i = 0; i < a; i++) {
+        sum += i;
+    }
+    return sum;
+};
+var x = prompt("input number")
+document.write("sum before(" + x +")=" + before(x));
