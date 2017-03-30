8ab820be18766298851a3ac90dff797d3e12da95
VanHakobyan <van19962013@mail.ru>
2017-03-30 19:29:09 +0400

    corrected and END

--- Array/Shift and Unshift.js
+++ Array/Shift and Unshift.js
@@ -1,1 +1,14 @@
+
+//shift
+document.write("<b>shift</b>" + "<br>");
 var array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+document.write("array " + array + "<br>");
+var value = array.shift();
+document.write("new array " + array + "<br>");
+document.write("deleted " + value + "<br>");
+
+//unshift
+document.write("<b>unShift</b> <br>");
+var length = array.unshift(0);
+document.write("new array " + array + "<br>");
+document.write("deleted " + length + "<br>");
