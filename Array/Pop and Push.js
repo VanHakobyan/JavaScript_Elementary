0cf52ff45311ddf41dafc7bee01c0467f9daa8a4
VanHakobyan <van19962013@mail.ru>
2017-03-30 19:20:04 +0400

    add pop push

---
+++ Array/Pop and Push.js
@@ -0,0 +1,11 @@
+var array = [1, 2, 3, 4, 5, 6];
+document.write(array + "<br>");
+document.write("lengt array " + array.length+"<br>");
+var length = array.push(7, 8, 9, 0);
+document.write(array + "<br>");
+document.write("lengt array " + length + "<br>");
+length = array.pop(2);
+document.write(array + "<br>");
+document.write("lengt array " + array.length + "<br>");
+document.write("deleted " + length + "<br>");
+
