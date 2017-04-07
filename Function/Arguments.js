423bc2448241792210f6a7675fa9d9614dbe6a02
VanHakobyan <van19962013@mail.ru>
2017-04-08 00:10:24 +0400

    add arguments

---
+++ Function/Arguments.js
@@ -0,0 +1,14 @@
+function Func(a, b, c)
+{
+    var maxFunc = Number.NEGATIVE_INFINITY;
+    for (var i = 0; i < arguments.length; i++)
+    {
+        if (arguments[i] > maxFunc)
+        {
+            maxFunc = arguments[i];
+        }
+    }
+    return maxFunc;
+}
+var res = Func(12, 1, 5, 4, 5, 8, 151, 678);
+document.write(res);//678
