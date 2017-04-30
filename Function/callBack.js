72c16cc60901bc006c8c4c2a6ba1d15818cd4d2b
VanHakobyan <van19962013@mail.ru>
2017-04-30 22:39:29 +0400

    add :ok:callBack

--- Function/callBack.js
+++ Function/callBack.js
@@ -3,6 +3,8 @@ var sub = function (a, b) { return a - b };
 
 function show(calBackFunction, a, b)
 {
-    calBackFunction(a, b);
-    return 
+    var resulit = callBackFunction(a, b);
+    document.write("<P>" + resulit);
 }
+show(add, 10, 20);
+show(sub, 10, 20);
