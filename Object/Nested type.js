4ce28e36f72d81bda73737cd06f1e1644a19c348
VanHakobyan <van19962013@mail.ru>
2017-05-02 22:29:06 +0400

    add:angel: nested type in JS :angel:

---
+++ Object/Nested type.js
@@ -0,0 +1,6 @@
+var obj = new Object();
+obj.name = "Van";
+obj.lastName = "Hakobyan";
+obj.YSU = new Object();
+obj.YSU.MHG = "5.2";
+document.write(obj.name + " " + obj.lastName + " " + obj.YSU.MHG);
